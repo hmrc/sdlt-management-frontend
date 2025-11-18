d23267aeded64128a8f3efea56a71c8ad389be86
tioadesola1 <162135798+tioadesola1@users.noreply.github.com>
2025-11-18 09:24:48 +0000

    Merge main

--- app/controllers/manage/DueForDeletionController.scala
+++ app/controllers/manage/DueForDeletionController.scala
@@ -16,21 +16,78 @@
 
 package controllers.manage
 
-import controllers.actions.IdentifierAction
-import javax.inject.{Inject, Singleton}
+import controllers.actions.*
 import play.api.i18n.I18nSupport
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
-import views.html.IndexView
+import controllers.routes.JourneyRecoveryController
+import models.responses.UniversalStatus
+import play.api.{Logger, Logging}
+import uk.gov.hmrc.govukfrontend.views.viewmodels.pagination.Pagination
+
+import javax.inject.{Inject, Singleton}
+import navigation.Navigator
+import utils.PaginationHelper
+import services.StampDutyLandTaxService
+import viewmodels.manage.SdltSubmittedReturnsViewModel
+import views.html.manage.{DueDeletion, SubmittedReturnsView}
+
+import java.time.LocalDate
+import scala.concurrent.{ExecutionContext, Future}
 
 @Singleton
 class DueForDeletionController @Inject()(
-                                             val controllerComponents: MessagesControllerComponents,
-                                             identify: IdentifierAction,
-                                             view: IndexView
-                                           ) extends FrontendBaseController with I18nSupport {
+                                            val controllerComponents: MessagesControllerComponents,
+                                            stampDutyLandTaxService: StampDutyLandTaxService,
+                                            identify: IdentifierAction,
+                                            getData: DataRetrievalAction,
+                                            requireData: DataRequiredAction,
+                                            stornRequiredAction: StornRequiredAction,
+                                            navigator: Navigator,
+                                            view: DueDeletion
+                                          )(implicit executionContext: ExecutionContext) extends FrontendBaseController with I18nSupport with Logging with PaginationHelper {
 
-  def onPageLoad(): Action[AnyContent] = identify { implicit request =>
-    Ok(view())
-  }
+
+
+  def onPageLoad(paginationIndex: Option[Int]): Action[AnyContent] =
+    (identify andThen getData andThen requireData andThen stornRequiredAction).async { implicit request =>
+
+      val urlSelector: Int => String = (paginationIndex: Int) => controllers.manage.routes.DueForDeletionController.onPageLoad(Some(paginationIndex)).url
+
+      val allDataRows: Future[List[SdltSubmittedReturnsViewModel]] =
+        Future.successful((0 to 17).toList.map(index =>
+          SdltSubmittedReturnsViewModel(
+            address = s"$index Riverside Drive",
+            agentReference = "B4C72F7T3",
+            dateSubmitted = LocalDate.parse("2025-04-05"),
+            utrn = "UTRN003",
+            purchaserName = "Brown",
+            status = UniversalStatus.SUBMITTED_NO_RECEIPT,
+            returnReference = "RETREF003",
+          )
+        ))
+
+      allDataRows.map {
+          case allDataRows =>
+            pageIndexSelector(paginationIndex, allDataRows.length) match {
+              case Right(selectedPageIndex) =>
+
+                val selectedPageIndex: Int = paginationIndex.getOrElse(1)
+                val paginator: Option[Pagination] = createPagination(selectedPageIndex, allDataRows.length, urlSelector)
+                val paginationText: Option[String] = getPaginationInfoText(selectedPageIndex, allDataRows)
+                val rowsForSelectedPage: List[SdltSubmittedReturnsViewModel] = getSelectedPageRows(allDataRows, selectedPageIndex)
+
+                Ok(view(rowsForSelectedPage, paginator, paginationText))
+
+              case Left(error) =>
+                Logger("application").error(s"[SubmittedReturnsController][onPageLoad] - paginationIndexError: $error")
+                Redirect(urlSelector(1))
+            }
+        } recover {
+        case ex =>
+          logger.error("[SubmittedReturnsController][onPageLoad] Unexpected failure", ex)
+          Redirect(JourneyRecoveryController.onPageLoad())
+      }
+    }
+
 }
