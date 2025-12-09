bc98c27191510f7fa41873338550fff42e1e88ba
Pavel Oliynyk <3528155+avelx@users.noreply.github.com>
2025-12-09 09:06:30 +0000

    Dtr 1797 dtr 1715 (#54)
    * DTR-1797:
      * apply changes required by DTR-1715
    
    * DTR-1797:
      * apply changes required by DTR-1715 [2]

--- test/controllers/manage/AtAGlanceControllerSpec.scala
+++ test/controllers/manage/AtAGlanceControllerSpec.scala
@@ -19,7 +19,6 @@ package controllers.manage
 import base.SpecBase
 import config.FrontendAppConfig
 import controllers.manage.routes.*
-import controllers.routes.JourneyRecoveryController
 import models.SdltReturnTypes.{IN_PROGRESS_RETURNS, IN_PROGRESS_RETURNS_DUE_FOR_DELETION, SUBMITTED_RETURNS_DUE_FOR_DELETION, SUBMITTED_SUBMITTED_RETURNS}
 import models.manage.AtAGlanceViewModel
 import models.responses.{SdltInProgressDueForDeletionReturnViewModel, SdltInProgressReturnViewModel, SdltReturnViewRow, SdltSubmittedDueForDeletionReturnViewModel, SdltSubmittedReturnViewModel}
@@ -32,7 +31,7 @@ import play.api.test.FakeRequest
 import play.api.test.Helpers.*
 import services.StampDutyLandTaxService
 import views.html.manage.AtAGlanceView
-
+import controllers.routes.{SystemErrorController}
 import scala.concurrent.Future
 
 class AtAGlanceControllerSpec
@@ -149,7 +148,7 @@ class AtAGlanceControllerSpec
 
         status(result) mustEqual SEE_OTHER
         redirectLocation(result).value mustEqual
-          JourneyRecoveryController.onPageLoad().url
+          SystemErrorController.onPageLoad().url
 
         verify(mockService, times(1)).getAgentCount(any(), any())
         verify(mockService, times(0)).getReturnsByTypeViewModel(any(), eqTo(IN_PROGRESS_RETURNS), any())(any())
